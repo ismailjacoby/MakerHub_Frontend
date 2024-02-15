f411f777fe87925d57466f9ec7dd22b57326d353
Jacoby Ismail <ismail.jacoby@gmail.com>
2024-02-15 19:38:12 +0100

    Created different page components & linked every link in the navbar and footer to the different page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {ServicesComponent} from "./services/services.component";
+import {ProductionsComponent} from "./productions/productions.component";
+import {SamplePacksComponent} from "./sample-packs/sample-packs.component";
+import {ContactComponent} from "./contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'services', component:ServicesComponent},
+  {path:'productions', component:ProductionsComponent},
+  {path:'samplepacks', component:SamplePacksComponent},
+  {path:'contact', component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductionsComponent } from './productions/productions.component';
+import { SamplePacksComponent } from './sample-packs/sample-packs.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProductionsComponent,
+    SamplePacksComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
