b33c41741d7bd8c2b5bd1544790995032aa2d9e3
ismailjacoby <ismail.jacoby.com>
2024-03-11 15:54:47 +0100

    upload Guard

--- src/app/components/productions/beats/beats.component.ts
+++ src/app/components/productions/beats/beats.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {isAdminGuard} from "../../../utils/guards/isAdminGuard";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-beats',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './beats.component.css'
 })
 export class BeatsComponent {
+
+    isAdmin = this._authService.isAdmin();
+
+    protected readonly isAdminGuard = isAdminGuard;
+
+    constructor(private _authService: AuthService) {
+
+    }
+
 
 }
--- src/app/components/productions/productions-routing.module.ts
+++ src/app/components/productions/productions-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UploadProductionsComponent} from "./upload-productions/upload-productions.component";
 import {BeatsComponent} from "./beats/beats.component";
+import {isAdminGuard} from "../../utils/guards/isAdminGuard";
 
 const routes: Routes = [
   {path:'', redirectTo: 'beats', pathMatch: 'full'},
   {path:'beats', component:BeatsComponent},
-  {path:'upload', component:UploadProductionsComponent}
+  {path:'upload', component:UploadProductionsComponent, canActivate: [isAdminGuard]}
 ];
 
 @NgModule({
