995b0dc29a398ca823f97d041aceb1eb430ed878
Jacoby Ismail <ismail.jacoby@gmail.com>
2024-03-14 19:48:20 +0100

    Update beats component style and Genre model
    Updated the styling for the beats component in order to provide a border and to handle overflow. The Genre model has been extended to support additional music genres such as 'CLUB' and 'RNB'.

--- src/app/models/Genre.ts
+++ src/app/models/Genre.ts
@@ -1,12 +1,14 @@
 export enum Genre{
   AFROBEATS = "AFROBEATS",
   AMAPIANO = "AMAPIANO",
+  CLUB = "CLUB",
   DANCEHALL = "DANCEHALL",
   DRILL = "DRILL",
   EDM = "EDM",
   JERSEY = "JERSEY",
   LATIN = "LATIN",
   POP = "POP",
+  RNB = "RNB",
   SHATTA = "SHATTA",
   TRAP = "TRAP"
 }
