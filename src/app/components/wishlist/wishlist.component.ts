0aaffa52789d0a7b3142623695be0a8fe02c35cc
ismailjacoby <ismail.jacoby.com>
2024-03-19 10:53:17 +0100

    Add wishlist functionality and UI
    A new service 'WishlistService' has been added which implements methods to fetch, add, and remove items from the wishlist by interacting with the server. The 'WishlistComponent' now uses this service to load wishlist items and allows to add them to the cart. Additionally, a new UI for the wishlist has been added showcasing the items with their cover image, title, price and an 'Add to Cart' button.

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ShoppingCartService} from "../../services/shopping-cart.service";
+import {WishlistService} from "../../services/wishlist.service";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-wishlist',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './wishlist.component.css'
 })
 export class WishlistComponent {
+  wishlistItems: any[] = [];
 
+  constructor(
+    private _shoppingCartService: ShoppingCartService,
+    private _wishlistService: WishlistService,
+    private _authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadWishlistItems();
+  }
+
+  addToCart(itemId: number): void {
+    console.log('Adding item to cart:', itemId);
+  }
+
+  loadWishlistItems(): void {
+    const username = this._authService.getUsername();
+    if (username) {
+      this._wishlistService.getWishlist(username).subscribe({
+        next: (items) => this.wishlistItems = items,
+        error: (error) => console.error('Failed to load wishlist items', error)
+      });
+    } else {
+      console.error('No user is currently logged in.');
+    }
+  }
 }
---
+++ src/app/services/wishlist.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {LicenseType} from "../models/LicenseType";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WishlistService {
+
+  private apiUrl = 'http://localhost:8080/wishlist';
+
+  constructor(private _http: HttpClient) {}
+
+  getWishlist(username: string): Observable<any> {
+    let params = new HttpParams().set('username', username);
+    return this._http.get<any>(`${this.apiUrl}/list`, { params });
+  }
+
+  addItemToWishlist(username: string, productionId?: number, samplePackId?: number): Observable<any> {
+    const params = new HttpParams()
+      .set('username', username)
+      .append('productionId', productionId ? productionId.toString() : '')
+      .append('samplePackId', samplePackId ? samplePackId.toString() : '');
+
+    return this._http.post(`${this.apiUrl}/add`, {}, { params });
+  }
+
+  removeItemFromWishlist(username: string, productionId?: number, samplePackId?: number): Observable<any> {
+    let params = new HttpParams().set('username', username);
+    if (productionId) {
+      params = params.set('productionId', productionId.toString());
+    } else if (samplePackId) {
+      params = params.set('samplePackId', samplePackId.toString());
+    }
+    return this._http.delete(`${this.apiUrl}/remove`, { params, responseType: 'text' });
+  }
+}
