06e4ea52ea49894e03b7873764262a2c3cb03ba6
ismailjacoby <ismail.jacoby.com>
2024-03-23 12:41:28 +0100

    Docker impl

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  stripe: "pk_test_51Iz1idIOjZy9IHl8JiuvZY7mGyykE6MXDDfi0TJlbaHlOToO8U8F65ZNEyofnWUpij7q5hshySdIE91RffHV3xNj00uvA4TYpB",
-  apiUrl: 'http://localhost:8080'
-};
